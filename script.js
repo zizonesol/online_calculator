c5bd19b97efbd4aa73881d415949185c60cf1a9b
David Park <parkhan1994@gmail.com>
2019-05-15 20:14:44 -0700

    Corrected the previous operation display

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 class Calculator
 {
-    constructor(previousOperandTextElement, currentOperandTextElement)
-        {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
             this.previousOperandTextElement = previousOperandTextElement
             this.currentOperandTextElement = currentOperandTextElement
             this.clear();
         }
 
-        clear()
-        {
+        clear() {
             this.currentOperand = ''
             this.previousOperand = ''
             this.operation = undefined
         }
 
-        delete()
-        {
+        delete() {
             this.currentOperand = this.currentOperand.toString().slice(0, -1)
         }
 
-        appendNumber(number)
-        {
+        appendNumber(number) {
             if (number === '.' && this.currentOperand.includes('.')) return
             this.currentOperand = this.currentOperand.toString() + number.toString()
         }
 
-        chooseOperation(operation)
-        {
+        chooseOperation(operation){
             if (this.currentOperand === '') return
-            if (this.previousOperand !== '') 
-            {
+            if (this.previousOperand !== '') {
                 this.compute()
             }
             this.operation = operation
@@ -37,12 +31,10 @@ class Calculator
             this.currentOperand = ''
         }
 
-        compute()
-        {
+        compute() {
             let computation
             const prev = parseFloat(this.previousOperand)
             const current = parseFloat(this.currentOperand)
-
             if (isNaN(prev) || isNaN(current)) return
             switch(this.operation)
             {
@@ -58,44 +50,27 @@ class Calculator
                 case '÷':
                     computation = prev / current
                     break
-                    /*
-                case '+/-':
-                    computation = computation * -1
-                    break
-                    */
                 default:
                     return
             }
-
             this.currentOperand = computation
             this.operation = undefined
             this.previousOperand = ''
         }
 
-        getDisplayNumber(number)
-        {
+        getDisplayNumber(number) {
             const stringNumber = number.toString()
             const integerDigits = parseFloat(stringNumber.split('.')[0])
             const decimalDigits = stringNumber.split('.')[1]
-
             let integerDisplay
-            if (isNaN(integerDigits))
-            {
+            if (isNaN(integerDigits)) {
                 integerDisplay = ''
+            } else {
+                integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
             }
-            else
-            {
-                integerDisplay = integerDigits.toLocaleString('en', 
-                {
-                    maximumFractionDigits: 0
-                })
-            }
-            if (decimalDigits != null)
-            {
+            if (decimalDigits != null) {
                 return `${integerDisplay}.${decimalDigits}`
-            }
-            else
-            {
+            } else {
                 return integerDisplay
             }
         }
@@ -107,7 +82,7 @@ class Calculator
             if (this.operation != null)
             {
                 this.previousOperandTextElement.innerText = 
-                    `${this.getDisplayNumber(previousOperand)} ${this.operation}`
+                    `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
             }
             else
             {
@@ -155,7 +130,6 @@ allClearButton.addEventListener('click', button =>
     calculator.clear()
     calculator.updateDisplay()
 })
-
 
 deleteButton.addEventListener('click', button =>
 {
